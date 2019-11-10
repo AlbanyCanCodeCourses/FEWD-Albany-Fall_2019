19b4ee38a18bb390ea59b3403e7b2c61c9d87dd7
ray-vazquez <rayvazquez125@gmail.com>
2019-11-10 11:49:28 -0500

    add week 9 directory and new javascript exersices

---
+++ exercises/js/11-5_ex2.js
@@ -0,0 +1,145 @@
+////////////////////////
+//////// Arrays ////////
+////////////////////////
+
+/**
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+ */
+
+/** Create an array with three names
+ * ie var names = [ name1, name, name3]
+ * @returns {array}
+ */
+
+var names = [];
+
+/**
+ * Create a second array with three different names
+ * @returns {array}
+ */
+
+var moreName = [];
+
+///////////////////////////////////
+////// Conditionals Statments /////
+///////////////////////////////////
+
+/**
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators
+ */
+
+/**
+ *
+ * If 3 is less than 5, print "three is less than five"
+ * @returns {string}
+ */
+
+/**
+ * If "5" plus "5" is equal to "five plus five" print "they are the same value and type".
+ * Otherwise, print "They are not the same value and type"
+ * @returns {string}
+ */
+
+/**
+ * Check if the length of the new filtered array is an even or odd number.
+ * If it is even, print "it is even".
+ * Otherwise print "it is odd".
+ * @returns {string}
+ */
+
+/**
+ * Write a program that prints the numbers from 1 to 100.
+ * For numbers divisible by 3, print “Fizz”.
+ * For numbers divisible by 5, print “Buzz”.
+ * For numbers divisible by both 3 and 5, print “FizzBuzz”
+ *
+ * @returns {string}
+ */
+
+///////////////////////////
+///////// Loops ///////////
+///////////////////////////
+
+/**
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration
+ */
+
+/**
+ * Create an array that combines both name arrays.
+ * Console log the names sorted alphabetically
+ *
+ * @returns {array}
+ */
+
+var combinedNames = [];
+
+// for () { }
+
+/**
+ * Remove all names in the combined names array that beging with the letters a, r, m
+ * @returns {array}
+ */
+
+/**
+ * For each item in the combined names array print a greeting message.
+ *
+ * @returns {string}
+ */
+
+////////////////////////
+/////// Functions //////
+////////////////////////
+
+/**
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions
+ */
+
+/**
+ * Create a function that retuns the sum of two parameters
+ * ie: var add = function (number, number) {
+ *   return number + number
+ * }
+ *
+ * @param
+ * @param
+ * @returns {number}
+ */
+
+/**
+ * Write a program that retuns the difference of two parameters
+ * @param
+ * @param
+ * @returns {number}
+ */
+
+/**
+ * Write a program that retuns the difference of two parameters
+ * @param
+ * @param
+ * @returns {number}
+ */
+
+/**
+ *Write a program that divides the return value of the add function by
+ * the return value of the subtract function
+ * @param
+ * @param
+ * @returns {number}
+ */
+
+/**
+ * Write a program that accepts one parameter.
+ * Print "it's hot outside" when the temperature is above 90 degrees.
+ * Print "the temperature is comfortable" if the temperature is between 50 and 90 degrees,
+ * Otherwise, it should print "it's to cold to go outside, it's a coding day! :)"
+ *
+ * @param {number}
+ * @returns {string}
+ */
+
+const weatherForcast = function() {};
